7d530462e37917b0f334d252a264aa14d89ebcac
Lily <alinecm@usp.br>
2025-03-29 19:50:23 -0300

    Mais funcionalidades
    socket, hashing e etc

--- Peer1_1.50/eachare.py
+++ Peer1_1.50/eachare.py
@@ -4,30 +4,85 @@ import os
 import time 
 import sys
 
+
 CLOCK = 0
 
-class vizinho:
-    def __init__(self, ip, port, status):
+SELF_IP = None
+SELF_PORT = None
+
+class Vizinho:
+    def __init__(self, iden, ip, port, status):
+        self.iden = iden
         self.ip = ip
         self.port = port
         self.status = status
+        self.clock = 0
 
     def setStatus(self, status):
         self.status = status
     
     def getStatus(self):
         return self.status
+    
+    def __hash__ (self):
+        return hash((self.iden))
+    
+    def __eq__(self, other):
+        if isinstance(other, Vizinho):
+            return self.iden == other.iden and self.ip == other.ip and self.port == other.port
+        return False
 
 
-def updateStatus(vizinhos, ip, port, newStatus):
 
-    vizinho = next((v for v in vizinhos if v.ip == ip and v.port == port), None)
 
+def searchVizinho(vizinhos, iden):
+    return next((v for v in vizinhos if v.iden == iden), None)
+
+def updateStatus(vizinhos, iden, newStatus):
+    vizinho = searchVizinho(vizinhos, iden)
     if vizinho:
+        vizinhos.remove(vizinho)
         vizinho.setStatus(newStatus)
-    else:
-        pass
+        vizinhos.add(vizinho)
 
+def updateClock(vizinhos, iden):
+    vizinho = searchVizinho(vizinhos, iden)
+    if vizinho:
+        vizinhos.remove(vizinho)
+        vizinho.clock += 1
+        vizinhos.add(vizinho)
+
+
+def sendMessage(ip, port, clock, message):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, int(port)))
+
+        self_ip_full = ":".join ([SELF_IP, SELF_PORT])
+        full_message = " ".join([self_ip_full, str(clock), message])
+
+        print(f"Encaminhando mensagem {full_message} para {ip}:{port}")
+        sock.sendall(full_message.encode())
+
+        sock.close()
+    except Exception as e:
+        print(f"Erro ao enviar mensagem para {ip}:{port} - {e}")
+
+
+def listener():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SELF_IP, int(SELF_PORT)))
+    server.listen(5)
+haiii
+    while True:
+        conn, addr = server.accept()
+        data = conn.recv(1024).decode()
+        peer_ip_port, clock, *arguments  = data.split(" ")
+        peer_ip, peer_port = peer_ip_port.split(":")
+        
+        print("Mensagem recebida: ")
+
+        conn.close()
 
 def openFile(path):
     with open(path, 'r') as file:
@@ -38,9 +93,7 @@ def openFile(path):
 
 
 if __name__ == "__main__":
-    
     print("Iniciando o programa...")
-
 
     #if len(sys.argv) != 3:
     #    print("Usage: python3 eachare.py <selfIP> <vizinhos> ")
@@ -48,23 +101,32 @@ if __name__ == "__main__":
     #selfIP = sys.argv[1]
     #vizinhos_filename = sys.argv[2]
 
-    selfIP = "26.204.92.82"
+    SELF_IP = "127.0.0.1"
+    SELF_PORT = "5000"
     
     vizinhos_filename = "vizinhos.txt"
     vizinhos_ips = openFile(vizinhos_filename).split("\n")
     vizinhos = set()
 
-
+    temp = 1
     for i in vizinhos_ips:
         ph = i.split(":")
-        v = vizinho(ph[0], ph[1], "OFFLINE")
+        v = Vizinho(temp, ph[0], int(ph[1]), "OFFLINE")
         vizinhos.add(v)
+        temp += 1
 
     print("Vizinhos:")
     for i in vizinhos:
-        print(i.ip + ":" + i.port)
+        print(f"[{str(i.iden)}] " + i.ip + ":" + str(i.port) + " - " + i.status + " - " + str(i.clock))
     
+    updateStatus(vizinhos, 1, "ONLINE")
+    updateClock(vizinhos, 2)
+    
+    print("Vizinhos:")
+    for i in vizinhos:
+        print(f"[{str(i.iden)}] "  + i.ip + ":" + str(i.port) + " - " + i.status + " - " + str(i.clock))
 
+    
 
     interface = 0
     while interface == 0:
@@ -79,6 +141,7 @@ if __name__ == "__main__":
             "   [9] - Sair\n"
         )
         option = input("> ")
+        print("aaaaaaaaa" + option)
         match option:
 
             case 1: # Listar peers
---
+++ Peer1_1.50/test.py
@@ -0,0 +1,20 @@
+
+
+print("aaaaaaa")
+
+thing = None
+
+def main():
+    anything()
+    thing = 3
+    print(thing)
+    thing = "Hello, World!"
+    print(thing)
+    print("This is a test.")
+
+def anything():
+    
+    thing = 2
+    print(thing)
+
+main()
